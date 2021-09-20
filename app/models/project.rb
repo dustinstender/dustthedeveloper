d8077452cb46ff3d1a54b9be675cd94492f2d139
Dustin Stender <dustinpstender@gmail.com>
2021-09-20 20:09:09 +0200

    fucked something up

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,4 @@ class Project < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 50, maximum: 400 }
   validates :url, presence: true
-  has_one_attached :image
 end
