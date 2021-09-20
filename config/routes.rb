af9791e497496de4ef2ae7ed17803770010c1478
Dustin Stender <dustinpstender@gmail.com>
2021-09-20 15:27:44 +0200

    nothing is broken here

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  resources :projects, only: [:index]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Adding Projects"
+Project.destroy_all
+Project.create({ title: 'Drug Dealer', description:
+  'Drug Dealer is a prototype web application meant for a mobile device so it is only mobile responsive. I created this
+   application with a team of three other students from Le Wagon as our end of the bootcamp project.
+   Our database only has pharmacies located in Paris, France.', url: 'https://www.drug-dealer.one' })
