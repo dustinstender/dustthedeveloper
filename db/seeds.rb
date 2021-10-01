1d7d4fd3206be13f65626d530b94ba07e4dce97a
Dustin Stender <dustinpstender@gmail.com>
2021-10-01 11:48:52 +0200

    more stuff for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,7 @@ puts "Adding Projects"
 Project.destroy_all
 Project.create!([{ title: 'Drug Dealer', description:
 'Drug Dealer is a prototype web application meant for a mobile device so it is only mobile responsive. I created this application with a team of three other students from Le Wagon as our end of the bootcamp project. Our database only has pharmacies located in Paris, France.',
-url: 'https://www.drug-dealer.one',
-image: 'drug-delaer.png',
-photo: File.new("app/assets/images/drug-delaer.png") },
+url: 'https://www.drug-dealer.one'},
 { title: 'Rent a Creature', description:
 'Rent a Creature is a fictional marketplace for magical creature rentals. Our database only has magical creatures living in Paris, France.',
-url: 'https://rentacreature-dustinstender.herokuapp.com/',
-image: 'rent-a-creature.png',
-photo: File.new("app/assets/images/rent-a-creature.png") }
-])
+url: 'https://rentacreature-dustinstender.herokuapp.com/'}])
