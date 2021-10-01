badc56d9434b8dadf1de52534d35e99a96f6abf5
Dustin Stender <dustinpstender@gmail.com>
2021-10-01 10:59:52 +0200

    deleted mailers

---
+++ test/mailers/previews/post_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/post_mailer
+class PostMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/post_mailer/post_created
+  def post_created
+    PostMailer.post_created
+  end
+
+end
